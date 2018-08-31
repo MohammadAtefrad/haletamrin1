cd14ab1c195bfe987942dd25a8995b900eb52e11
MohammadAtefrad2 <mo.atefrad@gmail.com>
2018-08-31 14:39:16 +0430

    define methods: 'setArray', 'getArray' & 'sortArray' in 'Class.ArraySet.php'.

--- Class.ArraySet.php
+++ Class.ArraySet.php
@@ -2,6 +2,23 @@
 
 class ArraySet{
 
-    public $array = new SplFixedArray(20);
+    public $len = 20;
+    public $array = new SplFixedArray($len);
+
+    public function setArray($index, $value){
+        $this->array[$index] = $value;
+    }
+
+    public function getArray($index){
+        if($index < $this->len){
+            return $this->array[$index];
+        }else{
+            return -1;
+        }
+    }
+
+    public function sortArray(){
+        return sort($this->array);
+    }
 
 }
